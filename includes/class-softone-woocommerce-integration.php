d7c29039a242ca1a4ffd91c8112de031873232ad
George Nicolaou <orionas.elite@gmail.com>
2025-11-16 22:49:05 +0200

    Handle main menu name matching flexibly

--- includes/class-softone-woocommerce-integration.php
+++ includes/class-softone-woocommerce-integration.php
@@ -112,7 +112,7 @@ class Softone_Woocommerce_Integration {
 			if ( defined( 'SOFTONE_WOOCOMMERCE_INTEGRATION_VERSION' ) ) {
 				$this->version = SOFTONE_WOOCOMMERCE_INTEGRATION_VERSION;
 			} else {
-				$this->version = '1.10.13';
+				$this->version = '1.10.14';
 			}
 
 			$this->plugin_name = 'softone-woocommerce-integration';
